61111c2d4f0686c3eb7e0fd4a4ebbf0b5d727f5c
Amom <amom.contato@gmail.com>
2025-12-16 14:21:09 -0300

    Started integrating POST method requisitions

--- my_project/home/views.py
+++ my_project/home/views.py
@@ -3,6 +3,13 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'landpage.html')
+    if request.method == "POST":
+        # Process the form data
+        nome = request.POST.get("nome")
+        email = request.POST.get("email")
+        mensagem = request.POST.get("mensagem")
+        return HttpResponse("Form submitted successfully by " + nome + "!")
+    else:
+        return render(request, 'landpage.html')
 
 # Create your views here.
