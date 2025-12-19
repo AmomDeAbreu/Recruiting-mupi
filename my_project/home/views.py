393ba98957a107d60744d37648b25a593db5486c
Amom Ao Cubo <102225054+AmomDeAbreu@users.noreply.github.com>
2025-12-19 18:00:31 +0000

    feature/started to create the adminstrators table onm db

--- my_project/home/views.py
+++ my_project/home/views.py
@@ -4,7 +4,17 @@ from .models import Usuario
 from django.shortcuts import render
 
 def login_view(request):
-    return render(request, 'login.html')
+    if request.method == "POST":
+        # Process the form data
+        nome = request.POST.get("nome")
+        senha = request.POST.get("senha")
+        user = Usuario.objects.filter(nome=nome, senha=senha)
+        if user.exists():
+            return HttpResponse("Login successful for " + nome + "!")
+        else:
+            return HttpResponse("Invalid username or password!")
+    else:
+        return render(request, 'login.html')
 
 
 def home(request):
@@ -18,5 +28,7 @@ def home(request):
         return HttpResponse("Form submitted successfully by " + nome + "!")
     else:
         return render(request, 'landpage.html')
+
+
 
 # Create your views here.
